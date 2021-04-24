b27c26770e67d4e117486e670123b6e3dbd6181f
Yrii <hvozdetskiyyrii@gmail.com>
2021-04-24 20:45:19 +0300

    ready

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// new CountdownTimer({
+//     selector: '#timer-1',
+//     targetDate: new Date('Aug 31, 2021'),
+// });
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const refs = {
+    clockface: document.querySelector('.js-clockface'),
+};
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+class CountdownTimer {
+    constructor({ onTick }) {
+        this.onTick = onTick;
+        this.targetDate = new Date('Aug 31, 2021');
+        this.interval = null;
+    }
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+    getTimeComponents(time) {
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
-});
+        return { days, hours, mins, secs };
+    }
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+
+    interval = setInterval(() => {
+        const currentTime = Date.now();
+        const deltaTime = this.targetDate - currentTime;
+        const time = this.getTimeComponents(deltaTime);
+        this.onTick(time);
+    }, 1000);
+}
+
+const time = new CountdownTimer({ onTick: updateClockface });
+
+function updateClockface({ days, hours, mins, secs }) {
+    refs.clockface.textContent = `${days}дн:${hours}год:${mins}хв:${secs}сек`;
+}
